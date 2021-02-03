0ab64e0db315a7cba9b62308ec75f10ea5097f46
Brian Foster <brianisbrilliant@gmail.com>
2021-02-03 15:20:10 -0600

    added pickups

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int coins = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("I have hit " + other.gameObject.name);
+        if(other.gameObject.CompareTag("Coin")) {
+            coins += 1;
+            // Destroy the coin
+            Destroy(other.gameObject);
+        }
+    }
+}
