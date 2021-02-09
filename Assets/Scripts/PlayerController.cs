0eefebd313ade880cf3e07483e18f226693e4a3d
SeoulP <55219779+SeoulP@users.noreply.github.com>
2021-02-08 22:04:16 -0600

    Added crouching functionality
    When pressing 'C', program will now toggle crouch by checking if you are already crouched or not, and then setting your local scale to .5 in the Y direction.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int oxygenSupply = 0;
+    bool crouch = false;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        crouching();
     }
 
     void OnTriggerEnter(Collider other)
@@ -25,6 +26,20 @@ public class PlayerController : MonoBehaviour
         {
             Destroy(other.gameObject);
             oxygenSupply++;
+        }
+    }
+
+    void crouching()
+    {
+        if (Input.GetKeyDown(KeyCode.C) && crouch == false)
+        {
+            transform.localScale = new Vector3(1, .5f, 1);
+            crouch = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.C) && crouch == true)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            crouch = false;
         }
     }
 }
