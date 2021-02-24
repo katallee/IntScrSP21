689a4bff4ab62e335234bbff8b27d88f6a4d3704
katallee <43423851+katallee@users.noreply.github.com>
2021-02-24 10:54:52 -0600

    miscellaneous changes

---
+++ Assets/Gun.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float bulletSpeed = 10f;     //couldn't get the code to work until I debugged and added f to 10
+    public Rigidbody bullet;
+
+    //this function will be called from the PlayerController.
+    public void Pickup(Transform hand) {
+        Debug.Log("I am running the Pickup() method.");
+        this.gameObject.transform.SetParent(hand);             //make gun follow hand
+        this.transform.localPosition = Vector3.zero;           //move gun to hand
+        this.transform.localRotation = Quaternion.identity;    //make gun face forward same as hand
+        this.GetComponent<Rigidbody>().isKinematic = true;     //make gun not fall
+        this.GetComponent<Collider>().enabled = false;                                  //turn off gun's collider
+        // other.GetComponent<RigidBody>().enabled = false;      //if you still getpushed around, add this
+    }
+
+    public void Use() {
+        Debug.Log("<color=red>Pow!</color>");
+        Rigidbody bulletClone = (Rigidbody) Instantiate(bullet, transform.position, transform.rotation);
+        bulletClone.velocity = transform.forward * bulletSpeed;
+        //code for public bulletSpeed, bullet, and bulletClone components found in Unity's community forum
+        //https://answers.unity.com/questions/581576/simple-bullet-script.html
+    }
+
+    public void Drop() {
+        Debug.Log("Dropping our item.");
+        this.gameObject.transform.SetParent(null);  //make gun stop following hand
+        this.GetComponent<Rigidbody>().isKinematic = false; //make gun fall
+        this.transform.Translate(0, 0, 2);  //move the gun forward 2 meters
+        this.GetComponent<Rigidbody>().AddForce(Vector3.forward * 10, ForceMode.Impulse);   //throw gun away with force
+        this.GetComponent<Collider>().enabled = true;   //turn on gun's collider
+    }
+}
+
+
+     
+     
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,21 +8,48 @@ public class PlayerController : MonoBehaviour
     bool crouch = false;
     public int coins = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
+    
+    //variables that should go at the top
+    [SerializeField]
+    Transform hand; //this is a hand positioned Empty child of Camera
+    
+    Gun heldItem;
+    
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetButtonDown("Fire1")) {
+            if(heldItem != null) { 
+                heldItem.Use();
+            } else {
+                Debug.Log("We aren't holding anything.");
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            if(heldItem != null) {
+                heldItem.Drop();
+                heldItem = null;
+            } else {
+                Debug.Log("We aren't holding anything.");
+            }
+        }
+
         crouching();
     }
 
+
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("I have hit " + other.gameObject.name + "!");
+        //Debug.Log("I have hit " + other.gameObject.name + "!");
+
+        if(other.gameObject.CompareTag("Item")) {
+            Debug.Log("I'm trying to pickup an item.");
+            //other.GetComponent<Gun>().Pickup(hand);
+            heldItem = other.GetComponent<Gun>();
+            heldItem.Pickup(hand);
+        }
+
         if(other.gameObject.CompareTag("Pickup"))
         {
             Destroy(other.gameObject);
