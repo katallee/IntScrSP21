3e76058d59bc93d0cd6f14632234f758f7a469da
katallee <43423851+katallee@users.noreply.github.com>
2021-03-24 12:51:34 -0500

    change scene and animation

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour, Threat
+{
+
+    [SerializeField]
+    Transform bulletSpawn;
+
+    bool handInView = false;
+
+    void Start()
+    {
+        if(bulletSpawn == null) {
+            bulletSpawn = this.transform.GetChild(0);
+        }
+    }
+
+    public void Fire(Transform Hand)
+    {
+        Debug.Log("<color=red>Die!</color>");
+        //TO DO 
+        //if statement to test if hand of player controller is facing cannon.
+        //if hand facing cannon bool handInView = true
+        if(handInView) {                                           //if canfire is true
+            GameObject ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            ball.transform.localScale = Vector3.one * 0.8f;
+            ball.transform.position = bulletSpawn.position;
+            ball.transform.Translate(transform.forward);    //move the ball forward by 1 meter
+
+            Rigidbody rb = ball.AddComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 50, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/Threat.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public interface Threat {
+    void Fire(Transform hand);
+}
