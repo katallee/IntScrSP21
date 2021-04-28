3b9d9ed154ae70dc15c1907f2f661964dc39dcc1
Kat Allee <katfrancesallee@gmail.com>
2021-04-28 14:20:37 -0500

    spawner limit

--- Assets/Scripts/AISpawner.cs
+++ Assets/Scripts/AISpawner.cs
@@ -9,18 +9,24 @@ public class AISpawner : MonoBehaviour
     public Transform goal;
     public float interval = 2;
 
+    public int spawnCount = 0;
+
     void Start()
     {
         StartCoroutine(SpawnAI());
     }
 
     IEnumerator SpawnAI() {
+        spawnCount += 1;
+
         while(true) {
-            yield return new WaitForSeconds(interval);
-            MoveTo copy = Instantiate(AIPrefab, 
+            while(spawnCount < 3) {
+                yield return new WaitForSeconds(interval);
+                MoveTo copy = Instantiate(AIPrefab, 
                                             this.transform.position, 
                                             this.transform.rotation);
-            copy.goal = goal;
+                copy.goal = goal;
+            }
         }
     }
 }
