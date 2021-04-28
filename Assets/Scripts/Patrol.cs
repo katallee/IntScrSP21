0d619ad61973dbec624d07eb7880ccc27c8348aa
Kat Allee <katfrancesallee@gmail.com>
2021-04-28 12:31:54 -0500

    final

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -41,8 +41,6 @@
         void Update () {
             // Choose the next destination point when the agent gets
             // close to the current one.
-            if (!agent.pathPending && agent.remainingDistance < 0.5f)
-                GotoNextPoint();
 
             Debug.Log("Distance to player: " + Vector3.Distance(this.transform.position, goal.transform.position));
 
